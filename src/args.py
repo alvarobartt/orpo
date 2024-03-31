c44f8504deb371595ae36e599a5c3ac10f27c97f
Alvaro Bartolome <alvarobartt@gmail.com>
2024-03-31 20:41:27 +0000

    Add `seed` in `args`

--- src/args.py
+++ src/args.py
@@ -23,6 +23,7 @@ def default_args(parser):
     parser.add_argument("--prompt_max_length", default=256, type=int)
     parser.add_argument("--response_max_length", default=1024, type=int)
     parser.add_argument("--alpha", default=1.0, type=float, help="Hyperparameter for weighting L_OR")
+    parser.add_argument("--seed", default=None, type=int)
 
     # Wandb Configurations
     parser.add_argument("--wandb_entity", default=None, type=str)
